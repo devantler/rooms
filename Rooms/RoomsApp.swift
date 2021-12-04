9f20004e4b296059516fc6c1ebcda3f70f73b778
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2021-12-04 17:03:17 +0100

    Initial Commit

---
+++ Rooms/RoomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomsApp.swift
+//  Rooms
+//
+//  Created by Nikolai Emil Damm on 04/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct RoomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
